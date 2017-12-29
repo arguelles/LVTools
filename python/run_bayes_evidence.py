fd693b40a60c48713b70296ef726182ad7c8afef
arguelles <arguelles@wisc.edu>
2017-12-28 23:06:39 -0600

    evidence

--- python/run_bayes_evidence.py
+++ python/run_bayes_evidence.py
@@ -1,49 +1,59 @@
 from __future__ import print_function
 import numpy as np
 import sys
+import time
+import pymultinest
 from pymultinest.solve import solve
+from pymultinest.watch import ProgressPrinter
 import lvsearchpy as lv
 
 #paths
-effective_area_path='/Users/carguelles/DropboxMIT/LVSearch/IC86OfficialRelease/effective_area_release/effective_area.h5'
-events_path='/Users/carguelles/DropboxMIT/LVSearch/carlos/data/simple_corrected_data_release.dat'
-chris_flux_path='/Users/carguelles/DropboxMIT/LVSearch/IC86OfficialRelease/effective_area_release/conventional_flux.h5'
-kaon_flux_path='/Users/carguelles/DropboxMIT/LVSearch/carlos/data/kaon_flux.h5'
-pion_flux_path='/Users/carguelles/DropboxMIT/LVSearch/carlos/data/pion_flux.h5'
-prompt_flux_path='/Users/carguelles/DropboxMIT/LVSearch/carlos/data/prompt_flux.h5'
+effective_area_path='../data/effective_area.h5'
+events_path='../data/simple_corrected_data_release.dat'
+chris_flux_path='../data/conventional_flux.h5'
+kaon_flux_path='../data/kaon_flux.h5'
+pion_flux_path='../data/pion_flux.h5'
+prompt_flux_path='../data/prompt_flux.h5'
 
 # constructing object
 lvsearch = lv.LVSearch(effective_area_path,events_path,chris_flux_path,kaon_flux_path,pion_flux_path,prompt_flux_path)
 lvsearch.SetVerbose(False)
 
-logRCmutau = -30; logICmutau = -30; logCmumu = -30;
+logRCmutau = -40; logICmutau = -40; logCmumu = -40;
 
-def CubePrior(theta):
-    normalization, cosmic_ray_slope, pik, prompt_norm, astro_norm, astro_gamma = theta
-    if(not( 0 < normalization < 10 and -0.5 < cosmic_ray_slope < 0.5 and 0. < pik < 2
-        and 0 < prompt_norm < 10 and 0 < astro_norm < 10 and -5 < astro_gamma < 5)):
-        return -np.inf
-    return 0
+parameters = ["normalization", "cosmic_ray_slope", "pik", "prompt_norm", "astro_norm", "astro_gamma"]
+#parameters_prior_ranges = [[0,10], [-0.5, 0.5], [0., 2], [0, 10], [0, 10], [-5, 5]]
+parameters_prior_ranges = [0,10,-0.5, 0.5,0.,2,0,10,0,10,-5,5]
+n_params = len(parameters)
+theta = np.zeros(n_params)
+prange = np.zeros(n_params)
 
-def lnProb(theta):
-    theta_ = theta[:]
-    # physics parameter test points
-    theta_.append(np.power(10.,logRCmutau))
-    theta_.append(np.power(10.,logICmutau))
-    theta_.append(np.power(10.,logCmumu))
-    output = lvsearch.llhFull(theta_)
-    return -output
+def CubePrior(cube, ndim, nparams):
+    # default are uniform priors
+    return ;
+
+def lnProb(cube, ndim, nparams):
+    for i in range(ndim):
+        prange[i] = parameters_prior_ranges[2*i+1] - parameters_prior_ranges[2*i]
+        theta[i] = prange[i]*cube[i] + parameters_prior_ranges[2*i]
+    theta_ = np.concatenate([theta,
+                             [np.power(10.,logRCmutau),np.power(10.,logICmutau),np.power(10.,logCmumu)]])
+    output = -lvsearch.llhFull(theta_)
+    return output
 
 ## Multinest business
 
 tt = time.time()
-parameters = ["normalization", "cosmic_ray_slope", "pik", "prompt_norm", "astro_norm", "astro_gamma"]
-prefix = str(logRCmutau) + "_" + str(logICmutau) + "_" + str(logCmumu)
-n_params = len(parameters)
-result = solve(LogLikelihood=lnProb, Prior=CubePrior, n_dims=n_params,
-               outputfiles_basename=prefix, verbose=True)
+prefix = "mnrun_" + str(logRCmutau) + "_" + str(logICmutau) + "_" + str(logCmumu)
+#progress = ProgressPrinter(n_params = n_params, outputfiles_basename=prefix); progress.start()
+result = pymultinest.run(LogLikelihood=lnProb, Prior=CubePrior, n_dims=n_params,
+                           outputfiles_basename=prefix, verbose=False)
+#progress.end()
 
-print()
-print('evidence: %(logZ).1f +- %(logZerr).1f' % result)
-print()
+analyzer = pymultinest.Analyzer(outputfiles_basename=prefix, n_params=n_params)
+
+a_lnZ = analyzer.get_stats()['global evidence']
+
+np.savez('lv_evidence_'+prefix+'.npy',logRCmutau=logRCmutau,logICmutau=logICmutau,logCmumu=logCmumu,a_lnZ=a_lnZ)
+print("Evidence ", a_lnZ)
 
